02989398c30788e5cb60febb638cdd0f755341ee
lucasthech <lucasszvchowski@gmail.com>
2024-11-01 15:43:07 -0300

    Consertado tudo

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
     let img = document.getElementsByClassName('download-button')[0];
-    if (window.innerWidth < 700) {
+    if (window.innerWidth < 768) {
         img.style.display = 'none';
-        document.getElementsByClassName('description2')[0].style.fontSize = '30px';
+        document.getElementsByClassName('description2')[0].style.fontSize = '25px';
+        document.getElementsByClassName('image-container')[0].style.display = 'none';
      }
      else{
         img.style.display = 'block';
-        document.getElementsByClassName('description2')[0].style.fontSize = '18px';
+        document.getElementsByClassName('description2')[0].style.display = 'none';
      }
