650a0a32056e39a1ffcc5157a0bcd0fd517aca09
lucasthech <lucasszvchowski@gmail.com>
2024-11-01 14:32:24 -0300

    Mais testes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-window.addEventListener('resize', function() {
+
     let img = document.getElementsByClassName('download-button')[0];
     if (window.innerWidth < 700) {
         img.style.display = 'none';
+        document.getElementsByClassName('description2')[0].style.fontSize = '50px';
      }
      else{
         img.style.display = 'block';
+        document.getElementsByClassName('description2')[0].style.fontSize = '18px';
      }
-   }); 
+
    
